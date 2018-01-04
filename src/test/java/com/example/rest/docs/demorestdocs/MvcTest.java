853332378bbbba0a87e563108ff4f26f90cfef65
Marcos Alves Cunha <alvesfc@protonmail.com>
2018-01-04 09:41:10 -0200

    Teste de geração de documentação

--- src/test/java/com/example/rest/docs/demorestdocs/MvcTest.java
+++ src/test/java/com/example/rest/docs/demorestdocs/MvcTest.java
@@ -9,17 +9,21 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.restdocs.SpringCloudContractRestDocs;
+import org.springframework.cloud.contract.wiremock.restdocs.WireMockRestDocs;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DemoRestDocsApplication.class)
-@AutoConfigureRestDocs(outputDir = "build/snippets")
+@SpringBootTest(classes = DemoRestDocsApplication.class,webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureRestDocs(value = "build/snippets")
 @AutoConfigureMockMvc
 public abstract class MvcTest {
 
@@ -35,6 +39,13 @@ public abstract class MvcTest {
 
     @Before
     public void setup() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/greeting?name=User")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"))
+                .andDo(MockMvcRestDocumentation.document("greeting",
+                        SpringCloudContractRestDocs.dslContract()));
+
         RestAssuredMockMvc.mockMvc(mockMvc);
     }
 }
